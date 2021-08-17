fc3032e7638b7d8d7a32e17c1160c19d61682a7f
RuslanNabokov <musaviruslan.1996@gmail.com>
2021-08-17 18:11:11 +0300

    s

--- ClassBaseTable.js
+++ ClassBaseTable.js
@@ -1,10 +1,78 @@
-import './data.js'
+import {data} from  './data.js'
+
+
+
+
+function StoreRow(conf={}){
+	var sel = this
+	this.id_arr =  conf.id_arr || []
+	this.data = conf.data || []
+	this.id = conf.id || 0
+	this.parrent = conf.parrent
+	this.data_ =  {}
+
+
+	this.data_gen  = function(){
+		
+		sel.id_arr.forEach( function(element,index){ 
+			sel.data_[element] = {}
+			
+			
+		})
+	}
+		
+	this.data_gen()
+
+function whicher(data){
+	Object.keys(data).forEach(key => {
+    	let internalValue = data[key]
+    
+
+   
+
+		    Object.defineProperty(data, key, {
+		        get() {
+		          
+		            return  this.parrent.data[key]
+		        },
+		        set(newVal) {
+		            this.parrent[key] = newVal
+		            
+		            
+		        }
+		    })
+		})
+
+
+}
+
+whicher(this.data_)
+
+
+}
+
+
+function  StoreCol(conf={}) {
+	this.id_arr =  conf.id_arr || []
+	this.data = conf.data || []
+	this.id = conf.id || 0
+	this.parrent = conf.parrent
+	
+
+
+}
+
+
+
 function BaseStore(conf={} ){
-	sel  = this
+	let sel  = this
+	console.log(conf)
 	this.name =  conf.name || 'Name'
 	this.type = conf.type  || 'local'
 	this.data_in = conf.data_in || []
 	this.data = {}
+	this.rows_ = []
+	this.cols = []
 	this.gen_ =  function (argument) {
     var text = "";
     var possible = "abcdefghijklmnopqrstuvwxyz";
@@ -15,22 +83,42 @@ function BaseStore(conf={} ){
 	}
 
 	
+	for(var i=0;i<sel.data_in[0].length;i++){ 
+		sel.cols[i] =  new StoreCol({'id':i,'parrent':this})  
+	}
 	this.data_in.forEach( function(element, index) {
-		let arr_hash = {} 
-		for(var i=0;i<element.length;i++){ sel.data[sel.gen_()] = element[i] }
+	
+		sel.rows_.push(new StoreRow({'data':{...element} ,'id':index, 'parrent':sel}))
+		let gen = ''
+		
+		for(var i=0;i<element.length;i++){ 
+			gen = sel.gen_()
+			sel.data[gen] = element[i]
+			
+			sel.rows_[index]['id_arr'][i] = gen 
+			
+			sel.cols[i].id_arr.push(gen)
+			sel.cols[i].data.push(element[i])
+
+
+
+		}
 	
 
-	});
+	})
+	sel.rows_.map(function(element,index){
+		element.data_gen()
+	})
+	;
 
-
-
-Object.keys(this.data).forEach(key => {
-    let internalValue = this.data[key]
+function whicher(data){
+Object.keys(data).forEach(key => {
+    let internalValue = data[key]
     
 
    
 
-    Object.defineProperty(this.data, key, {
+    Object.defineProperty(data, key, {
         get() {
           
             return internalValue
@@ -44,6 +132,10 @@ Object.keys(this.data).forEach(key => {
 })
 
 
+}
+
+
+whicher(this.data)
 
 
 
@@ -65,6 +157,16 @@ this._generate_id = function(){
 }
 
 
+// StoreRows.prototype = Object.create(BaseStore.prototype)
+// StoreCols.prototype = Object.create(BaseStore.prototype)
+// Object.defineProperty(StoreRows.prototype, 'constructor', {
+//     value: StoreRows,
+//     enumerable: false,
+//     writable: true });
+// Object.defineProperty(StoreCols.prototype, 'constructor', {
+//     value: StoreCols,
+//     enumerable: false,
+//     writable: true });
 
 
 function Create_table(conf={}){
@@ -86,7 +188,7 @@ function Create_table(conf={}){
 
 
 	this.sort_table_by = function(row){ 
- 		console.log(row)
+ 		
 		 let arr =  Array.from(this.table.rows).slice(1)
 		 arr.sort((a,b) => a.cells[row].innerHTML >  
 		 									 b.cells[row].innerHTML ? 1: -1   )
@@ -112,6 +214,7 @@ function Create_table(conf={}){
 			let  col_on_page = 0
 			let tr  = document.createElement('tr')
 			var sel = this
+			let td
 			Object.entries(this.data).forEach( function(element, index) {
 			
 			  if (col_on_page == sel.col_len ){
@@ -148,13 +251,13 @@ function Create_table(conf={}){
 
 
 let table2 =  new Create_table({
-								'data':bears
+								'data':data 
 											})
 table2.draw_table()
 
 table2.sort_table_by(2)
 
 
-let store = new BaseStore({'data_in':bears})
+let store = new BaseStore({'data_in':data })
 console.log(store)
 
